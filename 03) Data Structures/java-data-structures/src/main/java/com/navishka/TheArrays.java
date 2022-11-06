7579febc2caa38141e235ec46454c53461f022d9
Navishka Kularathna <navishka.randika@gmail.com>
2022-11-06 23:07:53 +0530

    Various methods of printing an array

--- 03) Data Structures/java-data-structures/src/main/java/com/navishka/TheArrays.java
+++ 03) Data Structures/java-data-structures/src/main/java/com/navishka/TheArrays.java
@@ -13,5 +13,21 @@ public class TheArrays {
         colors[2] = "yellow";
         System.out.println(Arrays.toString(colors));
 
+        int[] numbers = {100, 200};
+
+        for (int i = 0; i < colors.length; i++) {
+            System.out.println(colors[i]);
+        }
+
+        for (int i = colors.length - 1; i >= 0; i--) {
+            System.out.println(colors[i]);
+        }
+
+        for (String color: colors) {
+            System.out.println(color);
+        }
+
+        Arrays.stream(colors).forEach(System.out::println);
+
     }
 }
