0b2b5342968b02d2fee9f3b581a8bf70c705f5c0
Navishka Kularathna <navishka.randika@gmail.com>
2022-11-19 19:04:37 +0530

    Working with Maps

---
+++ 03) Data Structures/java-data-structures/src/main/java/com/navishka/WorkingWithMaps.java
@@ -0,0 +1,17 @@
+package com.navishka;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WorkingWithMaps {
+    public static void main(String[] args) {
+        Map<Integer, Person> map = new HashMap<>();
+        map.put(1, new Person("Alex"));
+        map.put(2, new Person("Alexa"));
+        map.put(3, new Person("Mariam"));
+
+        System.out.println(map);
+    }
+
+    record Person(String name) {}
+}
