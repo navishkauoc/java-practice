47ffeff9f5b8f8c1593e80999a5d87b9a6e0bf37
Navishka Kularathna <navishka.randika@gmail.com>
2022-11-22 22:00:15 +0530

    Working with Map methods

--- 03) Data Structures/java-data-structures/src/main/java/com/navishka/WorkingWithMaps.java
+++ 03) Data Structures/java-data-structures/src/main/java/com/navishka/WorkingWithMaps.java
@@ -10,7 +10,14 @@ public class WorkingWithMaps {
         map.put(2, new Person("Alexa"));
         map.put(3, new Person("Mariam"));
 
+//        map.put(3, new Person("Mariam 2"));
+
         System.out.println(map);
+        System.out.println(map.size());
+        System.out.println(map.get(1));
+        System.out.println(map.containsKey(4));
+        System.out.println(map.keySet());
+        System.out.println(map.entrySet());
     }
 
     record Person(String name) {}
