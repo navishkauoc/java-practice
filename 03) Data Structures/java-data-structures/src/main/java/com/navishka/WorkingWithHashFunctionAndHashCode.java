425049595fd8341eeaaeb86c552b674910f34842
Navishka Kularathna <navishka.randika@gmail.com>
2022-12-03 01:34:21 +0530

    Understanding Hash Function and HashCode

---
+++ 03) Data Structures/java-data-structures/src/main/java/com/navishka/WorkingWithHashFunctionAndHashCode.java
@@ -0,0 +1,47 @@
+package com.navishka;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class WorkingWithHashFunctionAndHashCode {
+
+    public static void main(String[] args) {
+        Map<Person, Diamond> map = new HashMap<>();
+        map.put(new Person("Jamila"), new Diamond("African Diamond"));
+
+        System.out.println(new Person("Jamila").hashCode());
+        System.out.println(new Person("Jamila").hashCode());
+        System.out.println(map.get(new Person("Jamila")));
+    }
+
+    static class Person {
+        String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+    }
+
+    record Diamond(String name) {}
+}
